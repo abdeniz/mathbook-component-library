94f895fe55f4f363341051d029af11451a24572c
Deniz Abdurrahmani <contact@denizabdurrahmani.com>
2021-06-07 22:17:32 +0300

    Button init commit

--- mathbook-component-library/src/components/button/Button.tsx
+++ mathbook-component-library/src/components/button/Button.tsx
@@ -1,65 +1,48 @@
 import React from "react";
 import styled from "styled-components";
-import { fontFamily } from "../styles";
+import { colors, fontFamily, fontSizing, spacing } from "../styles";
 
-export interface ButtonProps  {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: "small" | "medium" | "large";
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => void;
+export interface IButton  {
+  fullWidth?: boolean
 };
 
 /**
  * Primary UI component for user interaction
  */
 const Button = ({
-  primary = true,
-  backgroundColor,
-  size = "medium",
-  onClick,
-  label,
-}: ButtonProps) => {
-  const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
+  fullWidth = true
+}: IButton) => {
   return (
-    <StyledButton
-      type="button"
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " "
-      )}
-      style={backgroundColor ? { backgroundColor }: {}}
-      onClick={onClick}
-    >
-      {label}
-    </StyledButton>
+    <StyledButton>Button</StyledButton>
   );
 };
 
 export default Button;
 
 const StyledButton = styled.button`
+  //Reset styling
+  background: none;
+    border: 0;
+    color: inherit;
+    /* cursor: default; */
+    font: inherit;
+    line-height: normal;
+    overflow: visible;
+    padding: 0;
+    user-select: none;
+    -webkit-user-select: none; /* for button */
+       -moz-user-select: none;
+        -ms-user-select: none;
+
+  box-sizing: border-box;
+  cursor: pointer;
   width: 100%;
-  background-color: #4ECCA3;
+  background-color: ${colors.contrast};
   height: 60px;
-  padding: 0 16px;
+  padding: 0 ${spacing.double};
   font-family: ${fontFamily};
+  font-size: ${fontSizing.h3};
+  font-weight: 400;
+  color: ${colors.white1};
+  border-radius: ${spacing.default};
 `
--- mathbook-component-library/src/components/styles.ts
+++ mathbook-component-library/src/components/styles.ts
@@ -2,9 +2,25 @@ export const fontFamily = '"Poppins", sans-serif'
 export const rootEm = 16
 
 export const spacing = {
-    default: `${rootEm} px`,
-    half: `${rootEm / 2} px`,
-    quarter: `${rootEm / 4} px`,
-    double: `${rootEm * 2} px`,
-    threeQuarters: `${rootEm * 0.75} px`
+    default: `${rootEm}px`,
+    half: `${rootEm / 2}px`,
+    quarter: `${rootEm / 4}px`,
+    double: `${rootEm * 2}px`,
+    threeQuarters: `${rootEm * 0.75}px`
+}
+
+export const fontSizing = {
+    h1: `${rootEm * 2}px`,
+    h2: `${rootEm * 1.5}px`,
+    h3: `${rootEm * 1.25}px`,
+    paragraph: `${rootEm}px`,
+}
+
+export const colors = {
+    white1: '#EEEEEE',
+    white2: '#FAFAFA',
+    dark1: '#232931',
+    dark2: '#393E46',
+    contrast: '#4ECCA3',
+    error: '#FF5959',
 }
